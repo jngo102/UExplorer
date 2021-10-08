af2a076db3cbea4dbc54bdf4aec2f3db9963802a
Jason Ngo <jngo102@jngo102.localdomain>
2021-10-08 01:01:06 -0400

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Unity Explorer
+
+A wrapper around UnityExplorer, by sinai-dev:
+https://github.com/sinai-dev/UnityExplorer
+
+First created by 56:
+https://github.com/fifty-six
+
+Useful tool for mod developers to view how the game is structured and see how changes may be applied at runtime.
---
+++ UExplorer.cs
@@ -0,0 +1,42 @@
+﻿using Modding;
+using UnityEngine;
+using UnityExplorer;
+
+namespace UExplorer
+{
+    public class UExplorer : Mod
+    {
+        public override string GetVersion() => "1.0.0.0";
+
+        public UExplorer() : base("Unity Explorer") {}
+
+        public override void Initialize()
+        {
+            ExplorerStandalone.CreateInstance();
+            ExplorerStandalone.OnLog += OnLog;
+        }
+
+        private void OnLog(string message, LogType logType)
+        {
+            string prefix = "[Explorer] ";
+            switch (logType)
+            {
+                case LogType.Assert:
+                    LogFine(prefix + message);
+                    break;
+                case LogType.Error:
+                    LogError(prefix + message);
+                    break;
+                case LogType.Exception:
+                    LogDebug(prefix + message);
+                    break;
+                case LogType.Log:
+                    Log(prefix + message);
+                    break;
+                case LogType.Warning:
+                    LogWarn(prefix + message);
+                    break;
+            }
+        }
+    }
+}
